51efecd6466bbfbda944ab4d03ae591480f9142f
fei.liu <87204061@qq.com>
2016-08-21 21:47:45 +0800

    增加模式菜单

--- SmartShow/src/com/android/smartShow/fragment/MainPageTwoFragment.java
+++ SmartShow/src/com/android/smartShow/fragment/MainPageTwoFragment.java
@@ -1,9 +1,12 @@
 package com.android.smartShow.fragment;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import com.android.smartShow.R;
 import com.android.smartShow.adapter.ModelAdapter;
+import com.android.smartShow.adapter.ModelMenuAdapter;
+import com.android.smartShow.adapter.ModelMenuData;
 
 import android.os.Bundle;
 import android.os.Message;
@@ -12,18 +15,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 /**
  * 个人
  */
 public class MainPageTwoFragment extends BaseFragment implements OnClickListener {
-    private Button mType;
-    private Button mClass;
-    private Button mColor;
+    private Button mMenuFirst;
+    private Button mMenuSecond;
+    private Button mMenuThird;
     private ListView mList;
     private ModelAdapter mAdapter;
+    private ModelMenuAdapter mParentMenuAdapter;
+    private ModelMenuAdapter mSubMenuAdapter;
+    private ArrayList<ModelMenuData> mFirstMenuList = new ArrayList<ModelMenuData>();
+    private ArrayList<ModelMenuData> mSecondMenuList = new ArrayList<ModelMenuData>();
+    private ArrayList<ModelMenuData> mThirdMenuList = new ArrayList<ModelMenuData>();
+    private String mMenu;
+
+    private class MyOnItemCickListener implements OnItemClickListener {
+
+        private ListView mSubListView;
+
+        public MyOnItemCickListener(ListView listview) {
+            mSubListView = listview;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            // TODO Auto-generated method stub
+            mMenu = new StringBuilder().append(mParentMenuAdapter.getItem(arg2)).toString();
+            if (mParentMenuAdapter.isExpand(arg2)) {
+                mSubListView.setVisibility(View.VISIBLE);
+                mSubMenuAdapter = new ModelMenuAdapter(mParentMenuAdapter.getData(), getContext(), arg2);
+                mSubListView.setAdapter(mSubMenuAdapter);
+                mSubListView.setOnItemClickListener(mSubMenuListener);
+            }
+        }
+    }
+
+    private OnItemClickListener mSubMenuListener = new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            // TODO Auto-generated method stub
+            mMenu = new StringBuilder(mMenu).append("/").append(mSubMenuAdapter.getItem(arg2)).toString();
+        }
+    };
+
     private ModelAdapter.OnImageLoadListener mListener = new ModelAdapter.OnImageLoadListener() {
         @Override
         public void onImageLoad(int index) {
@@ -33,7 +76,28 @@ public class MainPageTwoFragment extends BaseFragment implements OnClickListener
 
     public void onCreate(Bundle savedInstanceState) {
         mAdapter = new ModelAdapter(getContext(), mListener);
+        initMenuList();
         super.onCreate(savedInstanceState);
+    }
+
+    private void initMenuList() {
+        // 测试数据
+        ArrayList<String> subMenu = new ArrayList<String>();
+        subMenu.add("小吃");
+        subMenu.add("火锅");
+        subMenu.add("烧烤");
+        ModelMenuData firstData = new ModelMenuData("美食", subMenu);
+        for (int i = 0; i < 10; i++) {
+            mFirstMenuList.add(firstData);
+        }
+
+        ModelMenuData seconddata = new ModelMenuData("红色", null);
+        for (int i = 0; i < 8; i++) {
+            mSecondMenuList.add(seconddata);
+        }
+
+        mThirdMenuList.add(new ModelMenuData("横屏", null));
+        mThirdMenuList.add(new ModelMenuData("竖屏", null));
     }
 
     @Override
@@ -75,23 +139,61 @@ public class MainPageTwoFragment extends BaseFragment implements OnClickListener
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
         View view = inflater.inflate(R.layout.main_page_two, container, false);
-        mType = (Button) view.findViewById(R.id.model_type);
-        mClass = (Button) view.findViewById(R.id.model_class);
-        mColor = (Button) view.findViewById(R.id.model_color);
+        mMenuFirst = (Button) view.findViewById(R.id.menu_first);
+        mMenuSecond = (Button) view.findViewById(R.id.menu_second);
+        mMenuThird = (Button) view.findViewById(R.id.menu_third);
         mList = (ListView) view.findViewById(R.id.model_list);
         String[] modelMenu = getResources().getStringArray(R.array.model_menu);
-        mType.setText(modelMenu[0]);
-        mClass.setText(modelMenu[1]);
-        mColor.setText(modelMenu[2]);
-        mType.setOnClickListener(this);
-        mClass.setOnClickListener(this);
-        mColor.setOnClickListener(this);
+        mMenuFirst.setText(modelMenu[0]);
+        mMenuSecond.setText(modelMenu[1]);
+        mMenuThird.setText(modelMenu[2]);
+        mMenuFirst.setOnClickListener(this);
+        mMenuSecond.setOnClickListener(this);
+        mMenuThird.setOnClickListener(this);
         mList.setAdapter(mAdapter);
         return view;
     }
 
+    private void showPopupMenu(View view, ArrayList<ModelMenuData> menuData) {
+        boolean isSubMenuShow = false;
+        for (ModelMenuData data : menuData) {
+            if (data.getIsExpand()) {
+                isSubMenuShow = true;
+                break;
+            }
+        }
+
+        View MenuView = LayoutInflater.from(getContext()).inflate(R.layout.model_menu_list, null);
+        ListView parentMenu = (ListView) MenuView.findViewById(R.id.parent_menu);
+        ListView subMenu = (ListView) MenuView.findViewById(R.id.sub_menu);
+        if (isSubMenuShow) {
+            subMenu.setVisibility(View.INVISIBLE);
+        } else {
+            subMenu.setVisibility(View.GONE);
+        }
+        mParentMenuAdapter = new ModelMenuAdapter(menuData, getContext());
+        parentMenu.setAdapter(mParentMenuAdapter);
+        parentMenu.setOnItemClickListener(new MyOnItemCickListener(subMenu));
+        PopupWindow pw = new PopupWindow(MenuView, LayoutParams.MATCH_PARENT, getView().getMeasuredHeight() - view.getMeasuredHeight());
+        pw.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.popup_full_dark));
+        pw.showAsDropDown(view);
+    }
+
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.menu_first:
+                showPopupMenu(view, mFirstMenuList);
+                break;
+            case R.id.menu_second:
+                showPopupMenu(view, mSecondMenuList);
+                break;
+            case R.id.menu_third:
+                showPopupMenu(view, mThirdMenuList);
+                break;
+            default:
+                break;
+        }
     }
 
 }
