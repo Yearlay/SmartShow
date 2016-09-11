7fdb449e66d41c10e55d9e250a7db8fa8bd1fb5f
yelei <yelei@DESKTOP-N7S5211>
2016-09-11 21:31:03 +0800

    添加门面的功能实现。

--- SmartShow/src/com/android/smartShow/fragment/MainPageOneFragment.java
+++ SmartShow/src/com/android/smartShow/fragment/MainPageOneFragment.java
@@ -7,13 +7,18 @@ import java.util.Map;
 import com.android.smartShow.R;
 import com.android.smartShow.adapter.ScreenAdapter;
 import com.android.smartShow.view.StoreTitleView;
+import com.project.template.utils.Log;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Message;
+import android.provider.AlarmClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 
@@ -27,6 +32,8 @@ public class MainPageOneFragment extends BaseFragment
     private ArrayList<StoreTitleView> mStoreItemList;
     private StoreTitleView mCurrentStoreTitleView;
     private GridView mScreenGridView;
+    
+    String[] mStoreListStr = {"门店一", "门店二"};
 
     public ArrayList<StoreTitleView> getStoreItemList() {
         return mStoreItemList;
@@ -85,16 +92,13 @@ public class MainPageOneFragment extends BaseFragment
         mStoreListLinearLayout = (LinearLayout) view.findViewById(R.id.store_list_linearlayout);
         
         mStoreItemList = new ArrayList<StoreTitleView>();
-        String[] storeListStr = getResources().getStringArray(R.array.store_list);
-        for (String storeItemStr : storeListStr) {
+        for (String storeItemStr : mStoreListStr) {
             StoreTitleView storeTitleView = new StoreTitleView(getContext());
             storeTitleView.setPageOneFragment(this);
             storeTitleView.getStoreTitle().setText(storeItemStr);
             mStoreItemList.add(storeTitleView);
         }
-        for (StoreTitleView storeTitleView : mStoreItemList) {
-            mStoreListLinearLayout.addView(storeTitleView);
-        }
+        reloadStore();
         if (mStoreItemList.size() > 0) {
             setCurrentStoreTitleView(mStoreItemList.get(0));
         }
@@ -102,11 +106,57 @@ public class MainPageOneFragment extends BaseFragment
         mScreenGridView = (GridView) view.findViewById(R.id.screen_gridview);
         mScreenGridView.setAdapter(new ScreenAdapter(getContext()));
         
+        view.findViewById(R.id.add_store_button).setOnClickListener(this);
+        
         return view;
+    }
+
+    private void reloadStore() {
+        while (mStoreListLinearLayout.getChildCount() != 1) {
+            mStoreListLinearLayout.removeViewAt(0);
+        }
+        
+        for (StoreTitleView storeTitleView : mStoreItemList) {
+            mStoreListLinearLayout.addView(storeTitleView,
+                    mStoreListLinearLayout.getChildCount() - 1);
+        }
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.add_store_button:
+                final EditText editText = new EditText(mActivity);
+                new AlertDialog.Builder(mActivity)
+                .setTitle(R.string.add_store_title)
+                .setView(editText)
+                .setPositiveButton(R.string.OK, 
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (editText.getText().length() != 0) {
+                                    StoreTitleView storeTitleView = new StoreTitleView(mActivity);
+                                    storeTitleView.setPageOneFragment(MainPageOneFragment.this);
+                                    storeTitleView.getStoreTitle().setText(editText.getText());
+                                    Log.d(TAG, "editText.getText():" + editText.getText());
+                                    mStoreItemList.add(storeTitleView);
+                                    
+                                    reloadStore();
+                                }
+                            }
+                        }
+                )
+                .setNegativeButton(R.string.CANCEL, 
+                        new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
